804f087b2ff8cd87993e64d7184641239fc626ee
balex69 <119514045+balex69@users.noreply.github.com>
2022-12-13 12:00:45 +0100

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,23 +8,23 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-    static final int SCREEN_WIDTH = 600, SCREEN_HEIGHT = 600, UNIT_SIZE = 25, DELAY = 70;
-    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-    final int x[] = new int[GAME_UNITS];
-    final int y[] = new int[GAME_UNITS];
-    int bodyParts = 6;
-    int applesEaten;
-    int appleX, appleY;
+    static final int ECRAN_LARGEUR = 600, ECRAN_HAUTEUR = 600, TAILLE_UNITES = 25, DELAY = 70;
+    static final int UNITES_JEU = (ECRAN_LARGEUR*ECRAN_HAUTEUR)/TAILLE_UNITES;
+    final int x[] = new int[UNITES_JEU];
+    final int y[] = new int[UNITES_JEU];
+    int corpsSerpent = 6;
+    int pommesMangees;
+    int pommeX, pommeY;
     char direction = 'D';
     boolean running = false;
     Timer timer;
     Random random;
-    private BufferedImage gameApple = ImageIO.read(getClass().getResource("/res/img/gameApple.png")); // (c) Freepik for all images
-    private BufferedImage snakeHead = ImageIO.read(getClass().getResource("/res/img/snakeHead.png"));
+    private BufferedImage imagePomme = ImageIO.read(getClass().getResource("/res/img/gameApple.png")); // (c) Freepik for all images
+    private BufferedImage imageSerpent = ImageIO.read(getClass().getResource("/res/img/snakeHead.png"));
     GamePanel() throws IOException {
 
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setPreferredSize(new Dimension(ECRAN_LARGEUR, ECRAN_HAUTEUR));
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
         this.addKeyListener(new GameKeyAdapter());
@@ -44,24 +44,24 @@ public class GamePanel extends JPanel implements ActionListener {
     public void draw(Graphics g) {
 
         if(running) {
-            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
-                //g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT); // utilisé pour prévoir le quadrillage
-                //g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            for (int i = 0; i < ECRAN_HAUTEUR / TAILLE_UNITES; i++) {
+                //g.drawLine(i * TAILLE_UNITES, 0, i * TAILLE_UNITES, ECRAN_HAUTEUR); // utilisé pour prévoir le quadrillage
+                //g.drawLine(0, i * TAILLE_UNITES, ECRAN_LARGEUR, i * TAILLE_UNITES);
             }
-            g.drawImage(gameApple, appleX, appleY, this);
+            g.drawImage(imagePomme, pommeX, pommeY, this);
 
-            for (int i = 0; i < bodyParts; i++) {
+            for (int i = 0; i < corpsSerpent; i++) {
                 if (i == 0) {
-                    g.drawImage(snakeHead, x[i], y[i], this);
+                    g.drawImage(imageSerpent, x[i], y[i], this);
                 } else {
                     g.setColor(new Color(0x75AF55));
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                    g.fillRect(x[i], y[i], TAILLE_UNITES, TAILLE_UNITES);
                 }
             }
             g.setColor(Color.red);
             g.setFont(new Font("Ink Free",Font.BOLD,25));
             FontMetrics metrics = getFontMetrics(g.getFont());
-            g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+            g.drawString("Score: "+pommesMangees, (ECRAN_LARGEUR - metrics.stringWidth("Score: "+pommesMangees))/2, g.getFont().getSize());
         }
         else {
             gameOver(g);
@@ -69,47 +69,47 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
     public void spawnApple() {
-        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
-        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+        pommeX = random.nextInt((int)(ECRAN_LARGEUR/TAILLE_UNITES))*TAILLE_UNITES;
+        pommeY = random.nextInt((int)(ECRAN_HAUTEUR/TAILLE_UNITES))*TAILLE_UNITES;
     }
     public void move() {
-        for(int i = bodyParts; i>0 ; i--) {
+        for(int i = corpsSerpent; i>0 ; i--) {
             x[i] = x[i-1];
             y[i] = y[i-1];
         }
         switch(direction) {
             case 'U':
-                y[0] = y[0] - UNIT_SIZE;
+                y[0] = y[0] - TAILLE_UNITES;
                 break;
             case 'D':
-                y[0] = y[0] + UNIT_SIZE;
+                y[0] = y[0] + TAILLE_UNITES;
                 break;
             case 'R':
-                x[0] = x[0] + UNIT_SIZE;
+                x[0] = x[0] + TAILLE_UNITES;
                 break;
             case 'L':
-                x[0] = x[0] - UNIT_SIZE;
+                x[0] = x[0] - TAILLE_UNITES;
                 break;
         }
     }
     public void checkApple() {
-        if ((x[0] == appleX) && (y[0] == appleY)) {
-            bodyParts++;
-            applesEaten++;
+        if ((x[0] == pommeX) && (y[0] == pommeY)) {
+            corpsSerpent++;
+            pommesMangees++;
             spawnApple();
         }
     }
     public void checkCollision() {
 
         // vérification que la tête ne croque pas le corps
-        for(int i = bodyParts; i>0; i--) {
+        for(int i = corpsSerpent; i>0; i--) {
             if((x[0] == x[i]) && (y[0] == y[i])) {
                 running = false;
             }
         }
 
         // vérification que la tête ne touche pas un bord
-        if((x[0] < 0) || (x[0] > SCREEN_WIDTH) || (y[0] < 0) || (y[0] > SCREEN_HEIGHT)) {
+        if((x[0] < 0) || (x[0] > ECRAN_LARGEUR) || (y[0] < 0) || (y[0] > ECRAN_HAUTEUR)) {
             running = false;
         }
 
@@ -122,17 +122,17 @@ public class GamePanel extends JPanel implements ActionListener {
         g.setColor(Color.red);
         g.setFont(new Font("Century Gothic",Font.BOLD,60));
         FontMetrics metricsGO = getFontMetrics(g.getFont());
-        g.drawString("Game Over! :(", (SCREEN_WIDTH - metricsGO.stringWidth("Game Over! :("))/2, SCREEN_HEIGHT/2);
+        g.drawString("Game Over! :(", (ECRAN_LARGEUR - metricsGO.stringWidth("Game Over! :("))/2, ECRAN_HAUTEUR/2);
 
         g.setColor(Color.red);
         g.setFont(new Font("Ink Free",Font.BOLD,25));
         FontMetrics metricsSC = getFontMetrics(g.getFont());
-        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metricsSC.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+        g.drawString("Score: "+pommesMangees, (ECRAN_LARGEUR - metricsSC.stringWidth("Score: "+pommesMangees))/2, g.getFont().getSize());
 
         g.setColor(Color.red);
         g.setFont(new Font("Ink Free",Font.BOLD,25));
         FontMetrics metricsRT = getFontMetrics(g.getFont());
-        g.drawString("Try again?", (SCREEN_WIDTH - metricsRT.stringWidth("Try again?"))/2, 400);
+        g.drawString("Try again?", (ECRAN_LARGEUR - metricsRT.stringWidth("Try again?"))/2, 400);
 
     }
 
